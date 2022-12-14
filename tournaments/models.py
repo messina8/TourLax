25cad7274081cb04ec07e875010097a63b4ded11
Tomás Messina <45271713+messina8@users.noreply.github.com>
2022-12-14 07:01:58 -0300

    updated tournament models

--- tournaments/models.py
+++ tournaments/models.py
@@ -5,10 +5,18 @@ from django.contrib.auth.models import User
 class Tournament(models.Model):
     name = models.CharField(max_length=50)
     players = models.IntegerField()
-    organizer = models.ManyToOneRel(User, on_delete=models.CASCADE, field_name="owner", to=User.username)
-    admins = models.ForeignKey(User, default=organizer, on_delete=models.SET_DEFAULT)
-    voters = models.ForeignKey(User, to=name, null=True, blank=True, on_delete=models.SET_NULL)
+    creator = models.ManyToOneRel(User, on_delete=models.CASCADE, field_name="owner", to=User.username)
+    organizer = models.CharField(max_length=70)
+    admins = models.ManyToManyRel(User, to=User.username)
+    voters = models.ManyToManyRel(User, to=User.username)
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=70)
+
 
 class Match(models.Model):
     tournament = models.ManyToOneRel(Tournament, on_delete=models.CASCADE, field_name='tournament', to=Tournament.name)
+    score = models.JSONField()
+    players = models.ManyToManyRel(Player)
 
