e2588fa62c18ae1c1ac038e317a5ab49e7fd1d33
Tomás Messina <45271713+messina8@users.noreply.github.com>
2022-12-14 08:54:23 -0300

    updated tournament models

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+import tournaments.models
+
 # Register your models here.
+
+admin.site.register(tournaments.models.Tournament)
+admin.site.register(tournaments.models.Player)
+admin.site.register(tournaments.models.Match)
--- tournaments/models.py
+++ tournaments/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Tournament(models.Model):
-    name = models.CharField(max_length=50)
+
+    class TournamentType(models.TextChoices):
+        LEAGUE = 'LE', _('League')
+        DIRECT_ELIMINATION = 'DE', _('Direct Elimination')
+
+    name = models.CharField(max_length=80)
     players = models.IntegerField()
     creator = models.ManyToOneRel(User, on_delete=models.CASCADE, field_name="owner", to=User.username)
     organizer = models.CharField(max_length=70)
     admins = models.ManyToManyRel(User, to=User.username)
     voters = models.ManyToManyRel(User, to=User.username)
+    type = models.CharField(max_length=2, choices=TournamentType.choices)
 
 
 class Player(models.Model):
@@ -18,5 +25,5 @@ class Player(models.Model):
 class Match(models.Model):
     tournament = models.ManyToOneRel(Tournament, on_delete=models.CASCADE, field_name='tournament', to=Tournament.name)
     score = models.JSONField()
-    players = models.ManyToManyRel(Player)
+    players = models.ManyToManyRel(Player, to=Player.name)
 
